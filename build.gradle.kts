86b4bcee715cda91536bd6e846b62add58fc9f42
0xSoul <31786518+0xSoul24@users.noreply.github.com>
2026-04-11 21:10:36 -0500

    Add Aikar's flags to runServer task and automatically accept server EULA

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,8 +41,36 @@ tasks.withType<Jar> {
 	enabled = false
 }
 
-tasks.runServer {
-    pluginJars(vanePlugins.map { it.tasks.findByName("copyJar")?.inputs?.files })
+
+tasks {
+	runServer {
+		pluginJars(vanePlugins.map { it.tasks.findByName("copyJar")?.inputs?.files })
+		jvmArgs(
+			"-Xms2G",
+			"-Xmx2G",
+			"-XX:+AlwaysPreTouch",
+			"-XX:+DisableExplicitGC",
+			"-XX:+ParallelRefProcEnabled",
+			"-XX:+PerfDisableSharedMem",
+			"-XX:+UnlockExperimentalVMOptions",
+			"-XX:+UseG1GC",
+			"-XX:G1HeapRegionSize=8M",
+			"-XX:G1HeapWastePercent=5",
+			"-XX:G1MaxNewSizePercent=40",
+			"-XX:G1MixedGCCountTarget=4",
+			"-XX:G1MixedGCLiveThresholdPercent=90",
+			"-XX:G1NewSizePercent=30",
+			"-XX:G1RSetUpdatingPauseTimePercent=5",
+			"-XX:G1ReservePercent=20",
+			"-XX:InitiatingHeapOccupancyPercent=15",
+			"-XX:MaxGCPauseMillis=200",
+			"-XX:MaxTenuringThreshold=1",
+			"-XX:SurvivorRatio=32",
+			"-Dusing.aikars.flags=https://mcflags.emc.gs",
+			"-Daikars.new.flags=true",
+			"-Dcom.mojang.eula.agree=true"
+		)
+	}
 }
 
 // Common settings to all subprojects.
